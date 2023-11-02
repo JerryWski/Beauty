7219f50b9daf04ca3166e4c3e9abea73a9826879
JerryWski <jarek_wasilewski@yahoo.com>
2023-11-02 22:01:50 +0100

    adding and styling gallery section

---
+++ src/components/GallerySection/Gallery.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './swiper.css';
+import './Gallery.css';
+// import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+const Gallery = () => {
+  return (
+    <section id='gallery' className='gallery-section'>
+      <p className='gallery-section-bg'>galeria</p>
+      <div className='gallery-welcome'>
+        <h2 className='gallery-welcome-header'>
+          Każdy zabieg, to nowa histora
+        </h2>
+        <p className='gallery-welcome-text'>
+          Odwiedź nas na&nbsp;
+          <a
+            className='gallery-welcome-text_link'
+            href='https://www.instagram.com/p/CoFEx07NS2H/'
+          >
+            Instagramie
+          </a>
+          &nbsp;i&nbsp;
+          <a
+            className='gallery-welcome-text_link'
+            href='https://www.facebook.com/SalonKosmetykiiKosmetologiiEstetycznej/'
+          >
+            Facebooku
+          </a>
+        </p>
+      </div>
+      <div className='gallery-container'>
+        <div className='swiper mySwiper'>
+          <div className='swiper-wrapper'>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img2.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img3.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img4.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img5.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img6.png' alt='beauty-salon' />
+            </div>
+            <div className='swiper-slide'>
+              <img src='../../public/gallery-img6.png' alt='beauty-salon' />
+            </div>
+          </div>
+          <div className='swiper-button-next'></div>
+          <div className='swiper-button-prev'></div>
+          <div className='swiper-pagination'></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
