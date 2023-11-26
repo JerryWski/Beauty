329f3b827e1d314e2497dde7d644d71aa2f0dc24
JerryWski <jarek_wasilewski@yahoo.com>
2023-11-26 18:16:30 +0100

    fixing path

--- src/components/TeamSection/TeamCard.tsx
+++ src/components/TeamSection/TeamCard.tsx
@@ -6,25 +6,28 @@ const teamCard = [
     id:1,
     image: '/person-small1.png',
     name: 'Dominika Wiśniewska',
-    link: '',
+    link: '/team#worker-one',
     info: 'Kobieta od zadań specjalnych. Dominika posiada nie tylko imponującą wiedzę z zakresu kosmetologii ale również doskonale wykorzystuje ją w swoich zdolnościach manualnych.',
   },
   {
     id:2,
     image: '/person-small3.png',
     name: 'Aleksandra Bojarowicz',
+    link: '/team#worker-two',
     info: 'Ola ponad wszystko ceni sobie zadowolenie klienta dlatego ustawiają się do niej kolejki. Niebywała kultura osobista i niezwykła gościnność to jej znak rozpoznawczy.',
   },
   {
     id:3,
     image: '/person-small4.png',
     name: 'Milena Jackowska',
+    link: '/team#worker-three',
     info: 'Kobieta „błyskawica”, która w ekspresowym tempie wykonuje usługi na najwyższym możliwym poziomie. Z wielką pasją tworzy najpiękniejsze rzęsy w Olsztynie ',
   },
   {
     id:4,
     image: '/person-small2.png',
     name: 'Kinga Wowk',
+    link: '/team#worker-four',
     info: '„Królowa paznokci” to jej drugie imię, ponieważ jej niebywała precyzja jest zachwycająca. Serdeczna i zawsze uśmiechnięta co sprawia, że po wizycie u niej czujesz zastrzyk endorfin',
   },
 ];
@@ -40,7 +43,7 @@ const TeamCard = () => {
           <div className='person-info'>
             <h3 className='person-info-title'>{team.name}</h3>
             <p className='person-info-text'>{team.info}</p>
-            <a href='/' className='person-info-link'>
+            <a href={team.link} className='person-info-link'>
               Czytaj więcej
             </a>
           </div>
