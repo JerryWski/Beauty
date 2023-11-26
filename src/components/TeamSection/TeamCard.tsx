14610f02cdd1cc269f16f5bb0452dbffbc1d8208
JerryWski <jarek_wasilewski@yahoo.com>
2023-11-26 18:04:12 +0100

    fixing path

--- src/components/TeamSection/TeamCard.tsx
+++ src/components/TeamSection/TeamCard.tsx
@@ -4,25 +4,25 @@ import  './TeamCard.css';
 const teamCard = [
   {
     id:1,
-    image: '../../src/assets/person-small1.png',
+    image: '/src/assets/person-small1.png',
     name: 'Dominika Wiśniewska',
     info: 'Kobieta od zadań specjalnych. Dominika posiada nie tylko imponującą wiedzę z zakresu kosmetologii ale również doskonale wykorzystuje ją w swoich zdolnościach manualnych.',
   },
   {
     id:2,
-    image: '../../src/assets/person-small3.png',
+    image: './src/assets/person-small3.png',
     name: 'Aleksandra Bojarowicz',
     info: 'Ola ponad wszystko ceni sobie zadowolenie klienta dlatego ustawiają się do niej kolejki. Niebywała kultura osobista i niezwykła gościnność to jej znak rozpoznawczy.',
   },
   {
     id:3,
-    image: '../../src/assets/person-small4.png',
+    image: './src/assets/person-small4.png',
     name: 'Milena Jackowska',
     info: 'Kobieta „błyskawica”, która w ekspresowym tempie wykonuje usługi na najwyższym możliwym poziomie. Z wielką pasją tworzy najpiękniejsze rzęsy w Olsztynie ',
   },
   {
     id:4,
-    image: '../../src/assets/person-small2.png',
+    image: './src/assets/person-small2.png',
     name: 'Kinga Wowk',
     info: '„Królowa paznokci” to jej drugie imię, ponieważ jej niebywała precyzja jest zachwycająca. Serdeczna i zawsze uśmiechnięta co sprawia, że po wizycie u niej czujesz zastrzyk endorfin',
   },
