9cf49d025c6173234314aac819d49c7c2c62f9e9
JerryWski <jarek_wasilewski@yahoo.com>
2024-02-22 17:29:04 +0100

    add google verf

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_f2282225.mjs';
+import { manifest } from './manifest_d7ae82c1.mjs';
 import 'react';
 import 'react-dom/server';
 import 'cookie';
@@ -14,13 +14,13 @@ import 'mime';
 import 'path-to-regexp';
 
 const _page0  = () => import('./chunks/generic_369323e4.mjs');
-const _page1  = () => import('./chunks/index_223a9a74.mjs');
-const _page2  = () => import('./chunks/robots_216e4bdd.mjs');
-const _page3  = () => import('./chunks/prices_4fbeaf63.mjs');
-const _page4  = () => import('./chunks/_slug__8ce1f095.mjs');
-const _page5  = () => import('./chunks/blog_dd32bd94.mjs');
-const _page6  = () => import('./chunks/shop_d72e48c3.mjs');
-const _page7  = () => import('./chunks/team_ccc71b8f.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/robots.txt.ts", _page2],["src/pages/prices.astro", _page3],["src/pages/blog/[slug].astro", _page4],["src/pages/blog.astro", _page5],["src/pages/shop.astro", _page6],["src/pages/team.astro", _page7]]);
+const _page1  = () => import('./chunks/index_00570abd.mjs');
+const _page2  = () => import('./chunks/robots_0f137c0d.mjs');
+const _page3  = () => import('./chunks/prices_2eb34008.mjs');
+const _page4  = () => import('./chunks/_slug__475a6913.mjs');
+const _page5  = () => import('./chunks/blog_6c467e8b.mjs');
+const _page6  = () => import('./chunks/shop_308a7a0d.mjs');
+const _page7  = () => import('./chunks/team_84fff1da.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/robots.txt.ts", _page2],["src/pages/prices.astro", _page3],["src/pages/blog/[slug].astro", _page4],["src/pages/blog.astro", _page5],["src/pages/shop.astro", _page6],["src/pages/team.astro", _page7]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
