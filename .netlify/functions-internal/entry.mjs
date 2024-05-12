645d2504e1aa5890794caf3414239ea2e3caa820
JerryWski <jarek_wasilewski@yahoo.com>
2024-05-12 17:39:30 +0200

    fixing emai

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_75e470e9.mjs';
+import { manifest } from './manifest_9db0509b.mjs';
 import 'react';
 import 'react-dom/server';
 import 'cookie';
@@ -14,14 +14,14 @@ import 'mime';
 import 'path-to-regexp';
 
 const _page0  = () => import('./chunks/generic_369323e4.mjs');
-const _page1  = () => import('./chunks/index_a96cc505.mjs');
-const _page2  = () => import('./chunks/polityka-prywatnosci_5012ac37.mjs');
-const _page3  = () => import('./chunks/robots_d05de5ad.mjs');
-const _page4  = () => import('./chunks/prices_cfa001d4.mjs');
-const _page5  = () => import('./chunks/_slug__221d036e.mjs');
-const _page6  = () => import('./chunks/blog_823ccb52.mjs');
-const _page7  = () => import('./chunks/shop_9aa118cc.mjs');
-const _page8  = () => import('./chunks/team_d81d6a42.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/polityka-prywatnosci.astro", _page2],["src/pages/robots.txt.ts", _page3],["src/pages/prices.astro", _page4],["src/pages/blog/[slug].astro", _page5],["src/pages/blog.astro", _page6],["src/pages/shop.astro", _page7],["src/pages/team.astro", _page8]]);
+const _page1  = () => import('./chunks/index_87fc170d.mjs');
+const _page2  = () => import('./chunks/polityka-prywatnosci_6b208951.mjs');
+const _page3  = () => import('./chunks/robots_204a8e95.mjs');
+const _page4  = () => import('./chunks/prices_338a1d90.mjs');
+const _page5  = () => import('./chunks/_slug__6f330e5f.mjs');
+const _page6  = () => import('./chunks/blog_a2829aaf.mjs');
+const _page7  = () => import('./chunks/shop_67efa06a.mjs');
+const _page8  = () => import('./chunks/team_8b6f1e30.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/polityka-prywatnosci.astro", _page2],["src/pages/robots.txt.ts", _page3],["src/pages/prices.astro", _page4],["src/pages/blog/[slug].astro", _page5],["src/pages/blog.astro", _page6],["src/pages/shop.astro", _page7],["src/pages/team.astro", _page8]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
