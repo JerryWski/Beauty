68098b8e27a90e758ced3ddff87a7b0e5b3dfefc
JerryWski <jarek_wasilewski@yahoo.com>
2024-07-10 07:58:48 +0200

    adding my new portfolio website

---
+++ .netlify/functions-internal/chunks/shop_23b7d646.mjs
@@ -0,0 +1,8 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./prerender_5a0b8195.mjs').then(n => n.s);
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_9db0509b.mjs';
+import { manifest } from './manifest_0e363436.mjs';
 import 'react';
 import 'react-dom/server';
 import 'cookie';
@@ -14,14 +14,14 @@ import 'mime';
 import 'path-to-regexp';
 
 const _page0  = () => import('./chunks/generic_369323e4.mjs');
-const _page1  = () => import('./chunks/index_87fc170d.mjs');
-const _page2  = () => import('./chunks/polityka-prywatnosci_6b208951.mjs');
-const _page3  = () => import('./chunks/robots_204a8e95.mjs');
-const _page4  = () => import('./chunks/prices_338a1d90.mjs');
-const _page5  = () => import('./chunks/_slug__6f330e5f.mjs');
-const _page6  = () => import('./chunks/blog_a2829aaf.mjs');
-const _page7  = () => import('./chunks/shop_67efa06a.mjs');
-const _page8  = () => import('./chunks/team_8b6f1e30.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/polityka-prywatnosci.astro", _page2],["src/pages/robots.txt.ts", _page3],["src/pages/prices.astro", _page4],["src/pages/blog/[slug].astro", _page5],["src/pages/blog.astro", _page6],["src/pages/shop.astro", _page7],["src/pages/team.astro", _page8]]);
+const _page1  = () => import('./chunks/index_7dcb5b4b.mjs');
+const _page2  = () => import('./chunks/polityka-prywatnosci_6aed120e.mjs');
+const _page3  = () => import('./chunks/robots_cc504723.mjs');
+const _page4  = () => import('./chunks/prices_96124531.mjs');
+const _page5  = () => import('./chunks/_slug__e200baba.mjs');
+const _page6  = () => import('./chunks/blog_305d2d41.mjs');
+const _page7  = () => import('./chunks/shop_9ef51458.mjs');
+const _page8  = () => import('./chunks/team_ff735eb6.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/polityka-prywatnosci.astro", _page2],["src/pages/robots.txt.ts", _page3],["src/pages/prices.astro", _page4],["src/pages/blog/[slug].astro", _page5],["src/pages/blog.astro", _page6],["src/pages/shop.astro", _page7],["src/pages/team.astro", _page8]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
